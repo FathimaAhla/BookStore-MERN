60f4c902285dfb1a307539f28e539865942dc32c
Ahla Fayis <139907384+FathimaAhla@users.noreply.github.com>
2024-01-08 21:57:02 +0530

    feat: change the mongoDBURL

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 5555;
 
-export const mongoDBURL = 'mongodb+srv://root:<password>@book-store.ygj0jml.mongodb.net/?retryWrites=true&w=majority'
+export const mongoDBURL = 'mongodb+srv://root:root@book-store-mern.gfz2mis.mongodb.net/books-collection?retryWrites=true&w=majority';
