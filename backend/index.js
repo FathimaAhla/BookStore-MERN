5ec73ab94dcbb841081f760325174ac3bfddeeae
Ahla Fayis <139907384+FathimaAhla@users.noreply.github.com>
2024-01-09 22:14:48 +0530

    feat: update a book with mongoose

--- backend/index.js
+++ backend/index.js
@@ -72,6 +72,37 @@ app.get('/books/:id', async (request, response) => {
     }
 });
 
+// Route for Update a book
+app.put('/books/:id', async (request, response) => {
+    try {
+        // validation
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+
+        ) {
+            return response.status(400).send({
+                message: 'Send all required field: title, author, publishYear',
+            });
+        }
+
+        const { id } = request.params;
+
+        const result = await Book.findByIdAndUpdate(id, request.body);
+
+        if (!result) {
+            return response.status(404).json({ message: ' Book not found' });
+        }
+
+        return response.status(500).send({ message: 'Book updated successfully' });
+
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+});
+
 mongoose
     .connect(mongoDBURL)
     .then(() => {
