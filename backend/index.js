33a99f977ec4fa9817c23cdd6a38589f81409892
Ahla Fayis <139907384+FathimaAhla@users.noreply.github.com>
2024-01-10 13:04:03 +0530

    CORS policy in node js and express js

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,25 @@ import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./modals/bookModel.js";
 import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
 
 const app = express();
 
 // Middleware for parsing request body
 app.use(express.json());
+
+// Middleware for handling CORS policy
+// option 1: Allow All Origins with Default of cors(*)
+app.use(cors())
+
+// Option 2: Allow Custom Origins
+app.use(
+    cors({
+        origin: 'http//localhost:3000',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type'],
+    })
+);
 
 app.get('/', (request, response) => {
     console.log(request)
