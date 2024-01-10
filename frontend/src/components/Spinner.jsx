3b19ab95488ee08202aef090e689399f7763b2d2
Ahla Fayis <139907384+FathimaAhla@users.noreply.github.com>
2024-01-11 00:01:20 +0530

    feat: create loading on spinner

---
+++ frontend/src/components/Spinner.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const CreateBook = () => {
+    return (
+        <div className="animate-plug w-16 h-16 m-8 rounded-full bg-sky-600"></div>
+    )
+}
+
+export default CreateBook;
